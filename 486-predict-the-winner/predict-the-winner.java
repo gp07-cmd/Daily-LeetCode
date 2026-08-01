6443f68417937213e22375d6f31baa89c1c89b3b
gp07-cmd <garvit0723@gmail.com>
2026-08-01 21:58:44 +0530

    Time: 65 ms (28.69%) | Memory: 42.7 MB (57.54%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        return getNetScore(nums, 0, nums.length - 1) >= 0;
+    }
+    
+    private int getNetScore(int[] nums, int i, int j) {
+        if (i == j) 
+            return nums[i];
+    
+        
+        int pickLeft = nums[i] - getNetScore(nums, i + 1, j);
+        int pickRight = nums[j] - getNetScore(nums, i, j - 1);
+        
+        return Math.max(pickLeft, pickRight);
+    }
+}

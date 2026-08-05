aadc1c17164d57bc5d704e5973d0985549d244d4
gp07-cmd <garvit0723@gmail.com>
2026-08-05 21:53:01 +0530

    Time: 65 ms (68.27%) | Memory: 290 MB (45.19%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -1,9 +1,5 @@
 class Solution {
-    public List<Integer> remainingMethods(int n, int k, int[][] inv) {
-        // remove all that are only invoked by `k` 
-        // and k should be 
-
-        // step 1. find all suspecius. 
+    public List<Integer> remainingMethods(int n, int k, int[][] inv) { 
         List<List<Integer>> adj = new ArrayList<>(); 
         for(int i = 0; i < n; i++) {
             adj.add(new ArrayList<>()); 
@@ -12,19 +8,13 @@ class Solution {
         for(int i = 0; i < inv.length; i++) {
             adj.get(inv[i][0]).add(inv[i][1]); 
         }
-
-        // Now find all elements that can be accesses from `k` 
         boolean vis[] = new boolean[n + 1]; 
         dfs(adj, k, vis); 
         boolean flag = true; 
-        // Now I need to check if there is any link with some outer node. 
         for(int i = 0; i < inv.length; i++) {
             int u = inv[i][0], v = inv[i][1]; 
-            if(!vis[u]) {
-                // outside grp 
+            if(!vis[u]) { 
                 if(vis[v]) {
-                    // inside grp 
-                    // then we don't remove anything. 
                     flag = false; 
                 }
             }
@@ -41,7 +31,7 @@ class Solution {
     private void dfs(List<List<Integer>> adj, int k, boolean[] vis) {
         if(vis[k]) return; 
 
-        vis[k] = true; // mark visited 
+        vis[k] = true; 
         for(Integer i: adj.get(k)) {
             if(!vis[i]) dfs(adj, i, vis); 
         }
